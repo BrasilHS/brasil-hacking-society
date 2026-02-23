fac520999ca7511cab9079ff95334035db91bc3b
Brasil Hacking Society <brasilhackingsociety@gmail.com>
2026-02-23 01:59:32 -0300

    The interface starts to kick in

--- app/routes/public.py
+++ app/routes/public.py
@@ -5,5 +5,13 @@ public_bp = Blueprint("public", __name__)
 
 @public_bp.route("/")
 def index():
-    posts = Post.query.order_by(Post.created_at.desc()).all()
+    # posts = Post.query.order_by(Post.created_at.desc()).all()
+    posts = [
+        {
+            "id": 1,
+            "title": "Espero que esteja gostando da exploração",
+            "content": "Por acaso você está tentando achar secrets nos commits passados? (já pode descer um commit haha). Talvez apenas curiosidade? não sei, enfim, tenha uma boa experiência e obrigado pela dedicação!",
+            "created_at": "23/02/2026 01:38:23"
+        }
+    ]
     return render_template("index.html", posts=posts)
